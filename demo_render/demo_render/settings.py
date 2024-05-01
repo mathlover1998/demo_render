88f8c6e9e96ff1ea3929ae5bcb918356bde0aa89
Cole Tran <44384202+mathlover1998@users.noreply.github.com>
2024-05-01 19:54:44 +0700

    add producttionstatic folder

--- demo_render/demo_render/settings.py
+++ demo_render/demo_render/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'demo_render.wsgi',
     'demo_render_app',
 ]
 
@@ -124,8 +123,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'productionstatic')
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = 'media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
